b3ecf48736a3edcfa398fae68f384ddf6dfd2c6a
Yumibyte <ashraigosa@gmail.com>
2020-04-05 12:47:50 -0700

    modified dataset and adjusted preprocessing

--- IsDrugAcceptable.py
+++ IsDrugAcceptable.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_excel('Data.xlsx')
+dataset = pd.read_csv('DataModified.csv')
 X = dataset.iloc[:, 2:9].values
 y = dataset.iloc[:, 9].values
 
