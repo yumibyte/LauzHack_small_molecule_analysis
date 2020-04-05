57e8346b744b98d5a0aceb0dc509424be32826d7
Yumibyte <ashraigosa@gmail.com>
2020-04-05 12:49:02 -0700

    prediction algorithm

--- IsDrugAcceptable.py
+++ IsDrugAcceptable.py
@@ -60,3 +60,10 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = [
 # Fitting ANN to Training set
 classifier.fit(X_train, y_train, batch_size = 5, nb_epoch = 4)
 
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
